d9cdc14f772a80a38ab5146fa2cc7f1367093af1
lizhuolun <li_zhuolun@126.com>
2026-07-23 19:27:13 +0100

    Initial Commit

---
+++ MeetingAssistant/MeetingAssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeetingAssistantApp.swift
+//  MeetingAssistant
+//
+//  Created by 李卓伦 on 2026/7/23.
+//
+
+import SwiftUI
+
+@main
+struct MeetingAssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MeetingAssistantTests/MeetingAssistantTests.swift
@@ -0,0 +1,17 @@
+//
+//  MeetingAssistantTests.swift
+//  MeetingAssistantTests
+//
+//  Created by 李卓伦 on 2026/7/23.
+//
+
+import Testing
+@testable import MeetingAssistant
+
+struct MeetingAssistantTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
